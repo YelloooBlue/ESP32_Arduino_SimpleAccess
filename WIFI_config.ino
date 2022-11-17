81ec9a267384b1363aa497615ddddf01c49b4ce8
YelloooBlue <58163429+YelloooBlue@users.noreply.github.com>
2022-11-17 17:16:04 +0800

    privacy

--- WIFI_config.ino
+++ WIFI_config.ino
@@ -6,8 +6,8 @@
 #include <WiFi.h> //ESP32下的WIFI库
 
 /*【参数配置】WIFI配置*/
-#define WIFI_SSID "818"       // WIFI名称
-#define WIFI_PSW "hlhxlwyhym" // WIFI密码
+#define WIFI_SSID "*********"       // WIFI名称
+#define WIFI_PSW "*********" // WIFI密码
 
 WiFiClient Client_WIFI; // WIFI客户端对象
 
